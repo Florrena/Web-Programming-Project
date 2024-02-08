4207b99d41e32519ca2fcd0d025a00f4a8eeb203
Asterxxxx <viktor.trilar@student.hamk.fi>
2024-02-08 23:06:24 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $title ?> </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;1,100&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<!-- Header -->
+    <header class="header-container">
+        <div class="page-header">
+            <div class="logo"><a href="home.php"><img src="images/logo.png"></a></div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="home.php">HOME</a></li>
+                    <li><a href="cats.php">CATS</a></li>
+                    <li><a href="shop.php">SHOP</a></li>
+                    <li><a href="about.php">ABOUT</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                    <div class="search-icon"><li><a href="#"><img src="images/search_.png"></a></li></div>
+                    <div class="shopping-icon"><li><a href="shopping-cart.html"><img src="images/shopping-cart.png"></a></li></div>
+                </ul>
+            </nav>
+        </div>
+    </header>
