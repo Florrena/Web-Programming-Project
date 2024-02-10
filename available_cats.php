ceab36c417d73d929e82c89396b8ac1c03cefc3b
Florrena <ania.bar25@gmail.com>
2024-02-10 15:59:42 +0200

    1

--- available_cats.php
+++ available_cats.php
@@ -10,12 +10,45 @@ include 'header.php'; ?>
                 <img src="images/catablebanner.jpg" alt="banner" draggable="false">
             </div>
 
+<!-- About body text -->
 <div class="article">
-  <h1> Available cats</h1>
-      <br><p>Welcome to our enchanting world of feline charm! Here, we present a delightful table showcasing a selection of the most endearing and cute cats that will undoubtedly steal your heart. Whether you're a seasoned cat lover or a newcomer to the realm of feline adoration, this table is bound to bring a smile to your face.
-      <br><br>Each of these cats possesses its own unique charm, from the Scottish Fold's adorable folded ears to the Maine Coon's majestic presence. Visit our cat haven, spend time with these lovable feline friends, and perhaps you'll find the purr-fect companion to bring home. Embrace the joy of feline companionship and let these cute cats add warmth and happiness to your life!
-      </p>
-       
-  </div>
-</div>
+                <h1> Available cats</h1>
+                    <br><p>Welcome to our enchanting world of feline charm! Here, we present a delightful table showcasing a selection of the most endearing and cute cats that will undoubtedly steal your heart. Whether you're a seasoned cat lover or a newcomer to the realm of feline adoration, this table is bound to bring a smile to your face.
+                    <br><br>Each of these cats possesses its own unique charm, from the Scottish Fold's adorable folded ears to the Maine Coon's majestic presence. Visit our cat haven, spend time with these lovable feline friends, and perhaps you'll find the purr-fect companion to bring home. Embrace the joy of feline companionship and let these cute cats add warmth and happiness to your life!
+                    </p>
+
+                    <div class="table-responsive">
+                        <table class="table">
+                          <thead>
+                            <tr class="table-primary">
+                              <th scope="col">#</th>
+                              <th scope="col">First Name</th>
+                              <th scope="col">Last Name</th>
+                              <th scope="col">Email</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr class="table-primary">
+                              <th scope="row">1</th>
+                              <td>Anna</td>
+                              <td>Barcikowska</td>
+                              <td>@outlook</td>
+                            </tr>
+                            <tr class="table-primary">
+                              <th scope="row">2</th>
+                              <td>Matti</td>
+                              <td>Thornton</td>
+                              <td>@gmail</td>
+                            </tr>
+                            <tr class="table-primary">
+                              <th scope="row">3</th>
+                              <td>Floczka</td>
+                              <td>Yori</td>
+                              <td>@discord</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                      
+    </div>
 <?php include 'footer.php'; ?>
