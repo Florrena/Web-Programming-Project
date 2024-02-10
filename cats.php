b12f2fec10d0e31bd046f7e8f882015504056704
Florrena <ania.bar25@gmail.com>
2024-02-10 16:23:09 +0200

    links

--- cats.php
+++ cats.php
@@ -17,42 +17,42 @@ include 'header.php'; ?>
                <div class="card">
                   <img src="images/bunker-buster.png" alt="Cat 1">
                   <h2>Bunker Buster</h2>
-                  <a href="cat-description.html"><button>See Profile</button></a>
+                  <a href="cat-description.php"><button>See Profile</button></a>
               </div>
               <div class="card">
                   <img src="images/stink-bomb.png" alt="Cat 2">
                   <h2>Stink Bomb</h2>
-                  <a href="cat-description.html"><button>See Profile</button></a>
+                  <a href="cat-description.php"><button>See Profile</button></a>
               </div>
               <div class="card">
                       <img src="images/chernobyl.png" alt="Cat 3">
                       <h2>Chernobyl</h2>
-                      <a href="cat-description.html"><button>See Profile</button></a>
+                      <a href="cat-description.php"><button>See Profile</button></a>
               </div>
               <div class="card">
                   <img src="images/herbert.jpeg" alt="Cat 4">
                   <h2>Herbert</h2>
-                  <a href="cat-description.html"><button>See Profile</button></a>
+                  <a href="cat-description.php"><button>See Profile</button></a>
               </div>
               <div class="card">
                   <img src="images/mafia-cat.jpg" alt="Cat 5">
                   <h2>Don Giovanni</h2>
-                  <a href="cat-description.html"><button>See Profile</button></a>
+                  <a href="cat-description.php"><button>See Profile</button></a>
               </div>
               <div class="card">
                   <img src="images/silly-cat.jpg" alt="Cat 6">
                   <h2>McSillypants Jr.</h2>
-                  <a href="cat-description.html"><button>See Profile</button></a>
+                  <a href="cat-description.php"><button>See Profile</button></a>
               </div>
               <div class="card">
                   <img src="images/Jinx.webp" alt="Cat 7">
                   <h2>Jinx</h2>
-                  <a href="cat-description.html"><button>See Profile</button></a>
+                  <a href="cat-description.php"><button>See Profile</button></a>
               </div>
               <div class="card">
                   <img src="images/meowke-tyson.png" alt="Cat 8">
                   <h2>Meowke Tyson</h2>
-                  <a href="cat-description.html"><button>See Profile</button></a>   
+                  <a href="cat-description.php"><button>See Profile</button></a>   
               </div>
           </div>
     </div>
