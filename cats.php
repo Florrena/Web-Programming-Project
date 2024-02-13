bba65506cdaf5cac607301443f3c478d0c26c734
walidmsahil <walidjahinsahil@gmail.com>
2024-02-13 13:43:50 +0200

    updated cats and cat description

--- cats.php
+++ cats.php
@@ -1,61 +1,40 @@
-<?php 
+<?php
 $title = 'Cats';
 include 'header.php'; ?>
 
 <!-- Container for body -->
 <div class="catnshop-container">
 
-<!-- Cats heading text -->
+    <!-- Cats heading text -->
     <div class="cat-header">
-          <img src="images/paw.webp">
-          <h1>Paw-fect Companions Await: Meet Our Adorable Cats</h1>
-          <img src="images/paw.webp">
-          </div>
-
-<!-- Cat cards -->          
-          <div class="cat-cards">
-               <div class="card">
-                  <img src="images/bunker-buster.png" alt="Cat 1">
-                  <h2>Bunker Buster</h2>
-                  <a href="cat-description.php"><button>See Profile</button></a>
-              </div>
-              <div class="card">
-                  <img src="images/stink-bomb.png" alt="Cat 2">
-                  <h2>Stink Bomb</h2>
-                  <a href="cat-description.php"><button>See Profile</button></a>
-              </div>
-              <div class="card">
-                      <img src="images/chernobyl.png" alt="Cat 3">
-                      <h2>Chernobyl</h2>
-                      <a href="cat-description.php"><button>See Profile</button></a>
-              </div>
-              <div class="card">
-                  <img src="images/herbert.jpeg" alt="Cat 4">
-                  <h2>Herbert</h2>
-                  <a href="cat-description.php"><button>See Profile</button></a>
-              </div>
-              <div class="card">
-                  <img src="images/mafia-cat.jpg" alt="Cat 5">
-                  <h2>Don Giovanni</h2>
-                  <a href="cat-description.php"><button>See Profile</button></a>
-              </div>
-              <div class="card">
-                  <img src="images/silly-cat.jpg" alt="Cat 6">
-                  <h2>McSillypants Jr.</h2>
-                  <a href="cat-description.php"><button>See Profile</button></a>
-              </div>
-              <div class="card">
-                  <img src="images/Jinx.webp" alt="Cat 7">
-                  <h2>Jinx</h2>
-                  <a href="cat-description.php"><button>See Profile</button></a>
-              </div>
-              <div class="card">
-                  <img src="images/meowke-tyson.png" alt="Cat 8">
-                  <h2>Meowke Tyson</h2>
-                  <a href="cat-description.php"><button>See Profile</button></a>   
-              </div>
-          </div>
+        <img src="images/paw.webp">
+        <h1>Paw-fect Companions Await: Meet Our Adorable Cats</h1>
+        <img src="images/paw.webp">
     </div>
 
+    <!-- Cat cards -->
+    <div class="cat-cards">
+        <?php
+        $cats = [
+            ['cat_id' => 1, 'name' => 'Bunker Buster', 'image' => 'images/bunker-buster.png'],
+            ['cat_id' => 2, 'name' => 'Stink Bomb', 'image' => 'images/stink-bomb.png'],
+            ['cat_id' => 3, 'name' => 'Chernobyl', 'image' => 'images/chernobyl.png'],
+            ['cat_id' => 4, 'name' => 'Herbert', 'image' => 'images/herbert.jpeg'],
+            ['cat_id' => 5, 'name' => 'Don Giovanni', 'image' => 'images/mafia-cat.jpg'],
+            ['cat_id' => 6, 'name' => 'McSillypants Jr.', 'image' => 'images/silly-cat.jpg'],
+            ['cat_id' => 7, 'name' => 'Jinx', 'image' => 'images/Jinx.webp'],
+            ['cat_id' => 8, 'name' => 'Meowke Tyson', 'image' => 'images/meowke-tyson.png'],
+        ];
+
+        foreach ($cats as $cat) {
+            echo '<div class="card">';
+            echo '<img src="' . $cat['image'] . '" alt="' . $cat['name'] . '">';
+            echo '<h2>' . $cat['name'] . '</h2>';
+            echo '<a href="cat-description-' . $cat['cat_id'] . '.php"><button>See Profile</button></a>';
+            echo '</div>';
+        }
+        ?>
+    </div>
+</div>
 
 <?php include 'footer.php'; ?>
